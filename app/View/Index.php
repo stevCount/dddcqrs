d7cff5a59f011f5143ae246a5519604f20f1233b
JS. Vique <jhonsteven707@gmail.com>
2024-02-13 00:17:41 -0500

    Cambios vista principal

--- app/View/Index.php
+++ app/View/Index.php
@@ -1,7 +1,7 @@
 <!-- copyright© Jhon S. Vique  -->
 <div class="container-fluid">
     <div class="text-center">
-        <h1>Plantila MVC</h1>
+        <h1>Plantila MCV con patron DDD Y CQRS</h1>
         <p>Copyright © Jhon S. Vique</p>
     </div>
 </div>
